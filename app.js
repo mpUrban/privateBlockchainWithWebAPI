d89005cfde5f0b70089862e96214a4e91dc62da3
Michael Urban <urban22@gmail.com>
2018-09-22 16:04:33 -0400

    added home redirect

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+
+//git remote add origin https://github.com/mpUrban/privateBlockchainWithWebAPI.git
+
+
 const express = require('express');
 const app = express();
 const port = 8000;
@@ -7,7 +11,7 @@ const path = require('path');
 
 const bodyParser = require('body-parser');
 
-const Blockchain = require('./blockchainClass'); 
+const Blockchain = require('./blockchainClass');
 const Block = require('./blockClass');
 
 // http://expressjs.com/en/guide/writing-middleware.html
@@ -25,27 +29,26 @@ let blockchain = new Blockchain;
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/home.html')));
 
-app.get('/block/:id',async (req,res)=>{
+app.get('/block/:id', async (req, res) => {
     const blockRes = await blockchain.getBlock(req.params.id);
-    if(blockRes) {
+    if (blockRes) {
         res.send(blockRes)
     } else {
         res.status(404).send("Block Not Found")
     }
-    });
+});
 
 //app.use(bodyParser.json()); // support json encoded bodies
 //app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-//*add redirect back to home
 
 
 
 
+// redirect to home - needs to be final redirect
+app.get('*', function (req, res) {
+    res.redirect('/');
+});
 
-
-
-
-
-    app.listen(port, 
-        () => console.log(`app listening on port ${port}!`));
+app.listen(port,
+    () => console.log(`app listening on port ${port}!`));
